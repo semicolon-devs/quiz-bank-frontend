83c182718b5172366f351075968ae1c9cb17b9d6
Hollin-A <ariyawansahollin@gmail.com>
2023-11-15 12:30:11 +0530

    feat: question page

--- app/(dashboard)/manage-questions/page.tsx
+++ app/(dashboard)/manage-questions/page.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 import {
   Table,
   TableHeader,
@@ -11,23 +14,24 @@ import {
 } from "@nextui-org/table";
 
 import { title } from "@/components/primitives";
+import { BASE_URL } from "@/config/apiConfig";
 
-const rows = [
-  {
-    key: "1",
-    question: "Question 01",
-    subject: "Biology",
-    subject_category: "Anatomy",
-    difficulty_level: "easy",
-  },
-  {
-    key: "2",
-    question: "Question 02",
-    subject: "Chemistry",
-    subject_category: "Organic Chemistry",
-    difficulty_level: "medium",
-  },
-];
+// const rows = [
+//   {
+//     key: "1",
+//     question: "Question 01",
+//     subject: "Biology",
+//     subject_category: "Anatomy",
+//     difficulty_level: "easy",
+//   },
+//   {
+//     key: "2",
+//     question: "Question 02",
+//     subject: "Chemistry",
+//     subject_category: "Organic Chemistry",
+//     difficulty_level: "medium",
+//   },
+// ];
 
 const columns = [
   {
@@ -39,12 +43,12 @@ const columns = [
     label: "SUBJECT",
   },
   {
-    key: "subject_category",
+    key: "subCategory",
     label: "SUBJECT CATEGORY",
   },
   {
-    key: "difficulty_level",
-    label: "DIFFICULTY LEVEL",
+    key: "difficulty",
+    label: "DIFFICULTY",
   },
   {
     key: "actions",
@@ -52,7 +56,70 @@ const columns = [
   },
 ];
 
+interface Question {
+  answers: any[];
+  correctAnswer: number[];
+  explaination: string;
+  question: string;
+  subCategory: string;
+  subject: { _id: string; name: string; subCategories: any[]; __v: number };
+  type: string;
+  __v: number;
+  _id: string;
+  difficulty: string;
+}
+
+interface QuestionRow {
+  _id: string;
+  question: string;
+  // subject: any;
+  // subCategory: any;
+  difficulty: string;
+}
+
 export default function ManageQuestionsPage() {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [questions, setQuestions] = useState<Question[]>([]);
+  const [questionRow, setQuestionRow] = useState<QuestionRow[]>([]);
+
+  console.log(questions);
+
+  useEffect(() => {
+    getQuestions();
+  }, []);
+
+  useEffect(() => {
+    setQuestionRow(extractQuestionRows(questions));
+  }, [questions]);
+
+  const getQuestions = () => {
+    setLoading(true);
+    const axiosConfig = {
+      method: "GET",
+      url: `${BASE_URL}questions`,
+    };
+    axios(axiosConfig)
+      .then((response) => {
+        setQuestions(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  const extractQuestionRows = (questions: Question[]): QuestionRow[] => {
+    return questions.map(({ _id, question, difficulty }) => ({
+      _id,
+      question,
+      difficulty,
+      // subject,
+      // subCategory,
+    }));
+  };
+
   return (
     <div>
       <h1 className={title({ size: "md" })}>Manage Questions</h1>
@@ -62,9 +129,9 @@ export default function ManageQuestionsPage() {
             <TableColumn key={column.key}>{column.label}</TableColumn>
           )}
         </TableHeader>
-        <TableBody items={rows}>
+        <TableBody items={questionRow}>
           {(item) => (
-            <TableRow key={item.key}>
+            <TableRow key={item._id}>
               {(columnKey) => (
                 <TableCell>{getKeyValue(item, columnKey)}</TableCell>
               )}
