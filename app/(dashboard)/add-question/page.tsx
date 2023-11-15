c0e90b6048f2e4e5b665b4abdf7d34e5e774e753
Hollin-A <ariyawansahollin@gmail.com>
2023-11-15 11:29:31 +0530

    feat: add question function added

--- app/(dashboard)/add-question/page.tsx
+++ app/(dashboard)/add-question/page.tsx
@@ -1,12 +1,15 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import { Checkbox } from "@nextui-org/checkbox";
 import { Button, ButtonGroup } from "@nextui-org/button";
 
 import { title } from "@/components/primitives";
+
+import { BASE_URL } from "@/config/apiConfig";
 
 const modules = {
   toolbar: [
@@ -24,8 +27,20 @@ const modules = {
 };
 
 const difficultyLevelArr = ["Easy", "Medium", "Hard"];
-const subjectArr = ["Biology", "Chemistry", "Mathematics"];
-const subjectCategoryArr = ["Anatomy", "Epidomology", "Physiology"];
+// const subjectArr = ["Biology", "Chemistry", "Mathematics"];
+// const subjectCategoryArr = ["Anatomy", "Epidomology", "Physiology"];
+
+interface Course {
+  _id: string;
+  name: string;
+  subCategories: any[];
+  __v: number;
+}
+
+interface SubCategory {
+  _id: string;
+  name: string;
+}
 
 export default function AddQuestionPage() {
   const [difficultyLevel, setDifficultyLevel] = useState<string>(
@@ -43,9 +58,16 @@ export default function AddQuestionPage() {
   const [answer04, setAnswer04] = useState<string>("");
   const [isA04Selected, setIsA04Selected] = useState<boolean>(false);
   const [answerExplaination, setAnswerExplaination] = useState<string>("");
+  const [courses, setCourses] = useState<Course[]>([]);
+  const [subjects, setSubjects] = useState<{ _id: string; name: string }[]>([]);
+  const [subjectCategories, setSubjectCategories] = useState<
+    { _id: string; name: string }[] | null
+  >([]);
+  const [loading, setLoading] = useState<boolean>(false);
 
   const answers = [
     {
+      number: 1,
       label: "Answer 01",
       selectedState: isA01Selected,
       setSelectedState: setIsA01Selected,
@@ -53,6 +75,7 @@ export default function AddQuestionPage() {
       setState: setAnswer01,
     },
     {
+      number: 2,
       label: "Answer 02",
       selectedState: isA02Selected,
       setSelectedState: setIsA02Selected,
@@ -60,6 +83,7 @@ export default function AddQuestionPage() {
       setState: setAnswer02,
     },
     {
+      number: 3,
       label: "Answer 03",
       selectedState: isA03Selected,
       setSelectedState: setIsA03Selected,
@@ -67,6 +91,7 @@ export default function AddQuestionPage() {
       setState: setAnswer03,
     },
     {
+      number: 4,
       label: "Answer 04",
       selectedState: isA04Selected,
       setSelectedState: setIsA04Selected,
@@ -75,8 +100,90 @@ export default function AddQuestionPage() {
     },
   ];
 
+  useEffect(() => {
+    getCourses();
+  }, []);
+
+  useEffect(() => {
+    extractSubjects();
+  }, [courses]);
+
+  useEffect(() => {
+    setSubjectCategories(getSubCategories(subject));
+  }, [subject]);
+
+  const extractSubjects = () => {
+    setSubjects(courses.map(({ _id, name }) => ({ _id, name })));
+  };
+
+  const getSubCategories = (subjectId: string): SubCategory[] | null => {
+    const subject = courses.find((s) => s._id === subjectId);
+
+    if (subject) {
+      return subject.subCategories.map(({ _id, name }) => ({ _id, name }));
+    }
+
+    return null;
+  };
+
+  const getCourses = () => {
+    setLoading(true);
+    const axiosConfig = {
+      method: "GET",
+      url: `${BASE_URL}subjects`,
+    };
+    axios(axiosConfig)
+      .then((response) => {
+        setCourses(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
   const handleAddQuestionButtonClick = () => {
-    console.log("btn click");
+    const getCorrectAnswer = () => {
+      const correctAnswerArr: number[] = [];
+      answers.map((answer) => {
+        if (answer.selectedState) {
+          correctAnswerArr.push(answer.number);
+        }
+      });
+      return correctAnswerArr;
+    };
+
+    setLoading(true);
+    const axiosConfig = {
+      method: "POST",
+      url: `${BASE_URL}questions`,
+      data: {
+        subject: subject,
+        subCategory: subjectCategory,
+        type: "MCQ",
+        question: question,
+        answers: [
+          { number: 1, answer: answer01 },
+          { number: 2, answer: answer02 },
+          { number: 3, answer: answer03 },
+          { number: 4, answer: answer04 },
+        ],
+        correctAnswer: getCorrectAnswer(),
+        explaination: answerExplaination,
+      },
+    };
+    axios(axiosConfig)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   };
 
   return (
@@ -96,9 +203,9 @@ export default function AddQuestionPage() {
         </select>
         <p className="font-semibold">Select Subject</p>
         <select value={subject} onChange={(e) => setSubject(e.target.value)}>
-          {subjectArr.map((item) => (
-            <option value={item} key={item}>
-              {item}
+          {subjects.map((item) => (
+            <option value={item._id} key={item._id}>
+              {item.name}
             </option>
           ))}
         </select>
@@ -107,11 +214,12 @@ export default function AddQuestionPage() {
           value={subjectCategory}
           onChange={(e) => setSubjectCategory(e.target.value)}
         >
-          {subjectCategoryArr.map((item) => (
-            <option value={item} key={item}>
-              {item}
-            </option>
-          ))}
+          {subjectCategories &&
+            subjectCategories.map((item) => (
+              <option value={item._id} key={item._id}>
+                {item.name}
+              </option>
+            ))}
         </select>
       </div>
       <div className="my-10">
