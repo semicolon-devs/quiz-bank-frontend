9f74583b676b955aa09a2d3fef5029a60cece2c2
Hollin-A <ariyawansahollin@gmail.com>
2023-11-17 17:38:28 +0530

    feat: home page redirection

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import NextLink from "next/link";
+import { redirect } from "next/navigation";
 
 export default function Home() {
+  redirect("/home");
+
   return (
-    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">Home</section>
+    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10"></section>
   );
 }
