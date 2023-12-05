efe1af5ba09e03a265b4dc9232d9a9bad49c1291
Hollin-A <ariyawansahollin@gmail.com>
2023-12-05 21:21:57 +0530

    feat: students dashbaord home page

--- components/icons.tsx
+++ components/icons.tsx
@@ -345,6 +345,24 @@ export const ProfileIcon = () => (
   </svg>
 );
 
+export const RightArrowIcon = (props: { classes: string }) => (
+  <svg
+    className={`${props.classes} text-white`}
+    aria-hidden="true"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 8 14"
+  >
+    <path
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      d="m1 13 5.7-5.326a.909.909 0 0 0 0-1.348L1 1"
+    />
+  </svg>
+);
+
 export const EditIcon = (props: { classes: string }) => (
   <svg
     className={props.classes}
