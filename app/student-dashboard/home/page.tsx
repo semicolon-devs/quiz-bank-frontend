ef82c562144054346feed47418f7f5222fafe832
Hollin-A <ariyawansahollin@gmail.com>
2023-12-05 15:44:11 +0530

    feat: basic student routes

---
+++ app/student-dashboard/home/page.tsx
@@ -0,0 +1,9 @@
+import { title } from "@/components/primitives";
+
+export default function DashboardHomePage() {
+  return (
+    <div>
+      <h1 className={title({ size: "md" })}>Student Dashboard</h1>
+    </div>
+  );
+}
