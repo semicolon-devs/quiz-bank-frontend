746ab44d144a2cfdf967280a88fbf53828196f74
Hollin-A <ariyawansahollin@gmail.com>
2024-02-07 01:41:05 +0530

    logout button

--- components/icons.tsx
+++ components/icons.tsx
@@ -366,6 +366,24 @@ export const ProfileIcon = () => (
   </svg>
 );
 
+export const LogoutIcon = () => (
+  <svg
+    className="w-5 h-5 text-white"
+    aria-hidden="true"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+  >
+    <path
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      d="M20 12H8m12 0-4 4m4-4-4-4M9 4H7a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h2"
+    />
+  </svg>
+);
+
 export const RightArrowIcon = (props: { classes: string }) => (
   <svg
     className={`${props.classes} text-white`}
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -3,18 +3,28 @@
 import React, { useState, useEffect } from "react";
 import NextLink from "next/link";
 import { usePathname } from "next/navigation";
+import { useRouter } from "next/navigation";
 import clsx from "clsx";
+
+import { LogoutIcon } from "./icons";
 
 import { menuItems } from "@/config/menuItems";
 
-import { getUserDetails, getUser } from "@/helpers/userDetails";
+import {
+  getUserDetails,
+  getUser,
+  clearUserDetails,
+} from "@/helpers/userDetails";
+import { clearAuthToken } from "@/helpers/token";
 import { MenuItems, UserDetails } from "@/types";
 import { UserRole } from "@/utils/enums";
+import { UrlSlugType } from "@/utils/enums/UrlSlug";
 
 type Props = { admin: boolean };
 
 export const Sidebar = (props: Props) => {
   const pathname = usePathname();
+  const router = useRouter();
 
   const [userDetails, setUserDetails] = useState<UserDetails | null>();
   const [role, setRole] = useState<UserRole>();
@@ -53,12 +63,23 @@ export const Sidebar = (props: Props) => {
   );
 
   return (
-    <div className="bg-blue h-screen w-max flex flex-col gap-4 pt-16">
-      {menuItems()
-        .filter((route: any) =>
-          route.users.some((u: string) => u === role)
-        )
-        .map((item) => renderLink(item))}
+    <div className="bg-blue h-screen w-max flex flex-col justify-between pt-16">
+      <div className="flex flex-col gap-4">
+        {menuItems()
+          .filter((route: any) => route.users.some((u: string) => u === role))
+          .map((item) => renderLink(item))}
+      </div>
+      <div
+        className="cursor-pointer px-5 flex gap-5 items-center py-2 w-64 mb-1"
+        onClick={() => {
+          clearUserDetails();
+          clearAuthToken();
+          router.push(UrlSlugType.LOGIN);
+        }}
+      >
+        <LogoutIcon />
+        <p className="text-white font-semibold uppercase">log out</p>
+      </div>
     </div>
   );
 };
