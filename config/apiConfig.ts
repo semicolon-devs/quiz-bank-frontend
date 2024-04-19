0fc9389936cae1d16e50f73133275a47e6f12916
SupulCFG <supuledirisinghe@gmail.com>
2024-04-19 12:54:09 +0530

    heroku backend

--- config/apiConfig.ts
+++ config/apiConfig.ts
@@ -1,2 +1,3 @@
 // export const BASE_URL: string = "http://localhost:8080/api/v1/";
-export const BASE_URL: string = "http://api.studymedicineitaly.com/api/v1/";
+// export const BASE_URL: string = "http://api.studymedicineitaly.com/api/v1/";
+export const BASE_URL: string = "qbank-backend-e948ea062275.herokuapp.com/api/v1/";
