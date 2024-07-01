cbe26cdf641c93bd77cfb3f632670660bc616e6d
Sanjana Wijenayake <56200533+SanaDev98@users.noreply.github.com>
2024-07-01 14:23:54 +0530

    login page spinner and feedbacks added

--- components/spinner/index.tsx
+++ components/spinner/index.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Spinner() {
   return (
-    <div className="h-full w-full items-center justify-center">
-      <div className="border-light h-16 w-16 animate-spin rounded-full border-6 border-t-blue-600" />
+    <div className="flex h-full w-full items-center justify-center">
+      <div className="h-5 w-5 animate-spin rounded-full border-4 border-light border-t-blue-600"></div>
     </div>
   );
 }
